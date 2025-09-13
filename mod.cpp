424954476b855cd091d4581fcaa2ae0e48155e25
OverlordZorn <dennisAzorn@gmail.com>
2025-09-13 02:49:14 +0200

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,26 +1,26 @@
-name = "Zeus Eden Interiors Community {version}";
+name = "ZEIC - Zeus Eden Interiors Community {version}";
 
 author = "Overlord Zorn";
 
-picture         = "img\ZEIC-Logo.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024
+picture         = "img\ZEIC-Workshop.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024
 hideName        = "false";                                                                                                                          // Hide the extension name
 hidePicture     = "false";                                                                                                                          // Hide the extension menu
 
-logoSmall       = "img\ZEIC-Logo-only.paa";                                                                                                    // Display next to the item added by the mod 64x64
-logo            = "img\ZEIC-Logo-only.paa";                                                                                                   // Logo displayed in the main menu 128x128
-logoOver        = "img\ZEIC-Logo-only.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
+logoSmall       = "img\ZEIC-transparent.paa";                                                                                                    // Display next to the item added by the mod 64x64
+logo            = "img\ZEIC-transparent.paa";                                                                                                   // Logo displayed in the main menu 128x128
+logoOver        = "img\ZEIC-transparent.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
 
-tooltip         = "Zeus Eden Interior - Community Version";
-tooltipOwned    = "Zeus Eden Interior - Community Version";                                                                                                          // Tool tip displayed when the mouse is left over, in the main menu
-overview        = "Zeus Eden Interior - Community Version";                 // Overview text, displayed from the extension menu
-description     = "Zeus Eden Interior - Community Version";                                                     // Probably in context with action
+tooltip         = "ZEIC - Zeus Eden Interior Community Version";
+tooltipOwned    = "ZEIC - Zeus Eden Interior Community Version";                                                                                                          // Tool tip displayed when the mouse is left over, in the main menu
+overview        = "ZEIC - Zeus Eden Interior Community Version";                 // Overview text, displayed from the extension menu
+description     = "ZEIC - Zeus Eden Interior Community Version";                                                     // Probably in context with action
 action          = "https://github.com/CVO-Org/ZEIC/";                                                                                                // Website URL, that can accessed from the expansions menu 
 actionName      = "GitHub";                                                                                                                        // label of button/tooltip in extension menu
 
-overviewPicture = "ZEIC-Logo-only.paa";
-overviewText = "Zeus Eden Interior - Community Version";
+overviewPicture = "ZEIC-Workshop.paa";
+overviewText = "ZEIC - Zeus Eden Interior Community Version";
 
-overviewFootnote = "Zeus Eden Interior - Community Version";
+overviewFootnote = "ZEIC - Zeus Eden Interior Community Version";
 
 
 // Color used for DLC stripes and backgrounds (RGBA)
